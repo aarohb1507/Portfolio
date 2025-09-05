451cb66cf35a3d6a76296787ce71c3616016f781
Aaroh Bhardwaj <103322439+aarohb1507@users.noreply.github.com>
2025-09-06 03:51:43 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-const sideMenu=document.querySelector('#sideMenu');
+document.addEventListener('DOMContentLoaded', () => {
+  const sideMenu = document.querySelector('#sideMenu');
+  const navBar = document.querySelector('nav');
 
-function openMenu(){
-  sideMenu.style.transform='translateX(-16rem)';
-}
-function closeMenu(){
-  sideMenu.style.transform='translateX(16rem)';
-}
+  function openMenu() {
+    sideMenu.style.transform = 'translateX(-16rem)';
+  }
+
+  function closeMenu() {
+    sideMenu.style.transform = 'translateX(16rem)';
+
+  }
+
+});
+
+
+  
